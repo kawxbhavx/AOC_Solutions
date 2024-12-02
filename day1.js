406360bd211c2e4321acfdcae97ef63c9ef83a4c
kawxbhavx <37347846+kawxbhavx@users.noreply.github.com>
2024-12-02 19:20:57 +0530

    day1 part1

--- day1.js
+++ day1.js
@@ -1,1 +1,23 @@
-document.querySelector('pre').innerText
+locationIDsInput = document.querySelector('pre').innerText
+locationIDs=locationIDsInput.split("\n")
+locationIDs1 = []
+locationIDs2 = []
+locationIDs.forEach(function(ids) {
+  idsArr = ids.replace(/  +/g, ' ').split(" ");
+  locationIDs1.push(parseInt(idsArr[0]));
+  locationIDs2.push(parseInt(idsArr[1]));
+});
+locationIDs1.sort();
+locationIDs2.sort();
+distances = [];
+locationIDs1.forEach(function(id, idIndex) {
+  distance = Math.abs(locationIDs2[idIndex] - id);
+  distances.push(distance);
+});
+totalDistance = 0;
+distances.forEach(function(distance) {
+  if(!isNaN(distance)) {
+    totalDistance = totalDistance + distance;
+  }
+});
+console.log(totalDistance);
