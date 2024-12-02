8953dd6f7f933edabf8d87457f24e0ba1c87f33e
BHAVIK KAWLI <37347846+kawxbhavx@users.noreply.github.com>
2024-12-02 23:52:39 +0530

    optimized for part2

--- day2.js
+++ day2.js
@@ -84,3 +84,89 @@ safeGradientsDirection.forEach(function(levelGradients, levelGradientsIndex) {
 console.log(safeGradients.length);
   
 //part2
+reportsInput = document.querySelector('pre').innerText;
+reportsStr=reportsInput.split("\n");
+reports = [];
+reportsStr.forEach(function(reportStr) {
+  levelsStr = reportStr.split(" ");
+  report = [];
+  levelsStr.forEach(function(levelStr) {
+    level = parseInt(levelStr);
+    if(!isNaN(level)) {
+      report.push(level);
+    }    
+  });
+  if(report.length>0) {
+    reports.push(report);
+  }
+});
+
+function isSafeReport(inputReport) {
+  levelGradients = [];
+  for(let i=0; i<inputReport.length-1; i++) {
+    levelGradients.push(inputReport[i]-inputReport[i+1]);
+  }
+  if(levelGradients.includes(0)) {
+    return false;
+  }
+  direction = 1;
+  if(levelGradients[0]<0) {
+    direction = -1;
+  }
+  let safeGradients = true;
+  for(let j=1; j < levelGradients.length; j++) {
+    if((direction * levelGradients[j])<0) {
+      safeGradients = false;
+      break;
+    }
+  }
+  if(safeGradients) {
+    absoluteLevelGradients = [];
+    levelGradients.forEach(function(levelGradient) {
+      absoluteLevelGradient = Math.abs(levelGradient);
+      if(!absoluteLevelGradients.includes(absoluteLevelGradient)) {
+        absoluteLevelGradients.push(absoluteLevelGradient);
+      }
+    });
+    absoluteLevelGradients.sort();
+    absoluteLevelGradients.reverse();
+    if(!(absoluteLevelGradients[0]>3)) {
+      return true;
+    } else {
+      return false;
+    }
+  } else {
+    return false;
+  }  
+}
+
+safeReports = [];
+safeReportsIndices = [];
+unSafeReports = [];
+unSafeReportsIndices = [];
+reports.forEach(function(report,reportIndex) {
+  if(isSafeReport(report)) {
+    safeReports.push(report);
+    safeReportsIndices.push(reportIndex);
+  } else {
+    unSafeReports.push(report);
+    unSafeReportsIndices.push(reportIndex);
+  }
+});
+
+unSafeReports.forEach(function(report,reportIndex) {
+  for(let i=0; i<report.length; i++) {
+    for(let j=0; j<report.length; j++) {
+      modifiedReport = [];
+      if(i!=j) {
+        modifiedReport.push(report[j]);
+      }
+    }
+    if(isSafeReport(modifiedReport)) {
+      safeReports.push(report);
+      safeReportsIndices.push(unSafeReportsIndices[reportIndex]);      
+      break;
+    }      
+  }
+});
+console.log(safeReports.length);
