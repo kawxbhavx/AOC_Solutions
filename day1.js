e41aa9506cde3e1a66cf20b00a42f0f8d46b8ffe
kawxbhavx <37347846+kawxbhavx@users.noreply.github.com>
2024-12-02 19:46:10 +0530

    part2 solution

--- day1.js
+++ day1.js
@@ -1,7 +1,8 @@
-locationIDsInput = document.querySelector('pre').innerText
-locationIDs=locationIDsInput.split("\n")
-locationIDs1 = []
-locationIDs2 = []
+//Part1
+locationIDsInput = document.querySelector('pre').innerText;
+locationIDs=locationIDsInput.split("\n");
+locationIDs1 = [];
+locationIDs2 = [];
 locationIDs.forEach(function(ids) {
   idsArr = ids.replace(/  +/g, ' ').split(" ");
   locationIDs1.push(parseInt(idsArr[0]));
@@ -21,3 +22,31 @@ distances.forEach(function(distance) {
   }
 });
 console.log(totalDistance);
+
+
+//Part2
+locationIDsInput = document.querySelector('pre').innerText
+locationIDs=locationIDsInput.split("\n")
+locationIDs.pop();
+locationIDs1 = [];
+locationIDs2 = [];
+locationIDs.forEach(function(ids) {
+  idsArr = ids.replace(/  +/g, ' ').split(" ");
+  locationIDs1.push(parseInt(idsArr[0]));
+  locationIDs2.push(parseInt(idsArr[1]));
+});
+frequencies  = [];
+locationIDs1.forEach(function(id1) {
+  idCount = 0;
+  locationIDs2.forEach(function(id2) {
+    if(id1 === id2) {
+      idCount++;
+    }
+  });
+  frequencies.push(idCount);
+});
+similarityScores = 0;
+locationIDs1.forEach(function(id1, idIndex) {
+  similarityScores = similarityScores + (id1 * frequencies[idIndex]);
+});
+console.log(similarityScores);
