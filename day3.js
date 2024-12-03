008065354739e3eb68a27e959c34a02cefbfa11c
BHAVIK KAWLI <37347846+kawxbhavx@users.noreply.github.com>
2024-12-03 15:34:27 +0530

    Update day3.js

--- day3.js
+++ day3.js
@@ -1,1 +1,9 @@
 //part1
+instructionsInput = document.querySelector('pre').innerText;
+instructions = [];
+let pointer=0;
+while(pointer<instructionsInput.length) {
+  matchingIndex = instructionsInput.indexOf("mul(", pointer);
+  instructions.push(instructionsInput.substring(matchingIndex, matchingIndex+30));
+  pointer = pointer + "mul(".length;
+}
