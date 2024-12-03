91a84aa2ce8d24ae882e5cf2e06ebc31ef98467c
BHAVIK KAWLI <37347846+kawxbhavx@users.noreply.github.com>
2024-12-03 23:16:50 +0530

    Update day3.js

--- day3.js
+++ day3.js
@@ -2,8 +2,12 @@
 instructionsInput = document.querySelector('pre').innerText;
 instructions = [];
 let pointer=0;
+//debugger;
+let startMul="mul(";
+let maxPattern="999,999)";
 while(pointer<instructionsInput.length) {
-  matchingIndex = instructionsInput.indexOf("mul(", pointer);
-  instructions.push(instructionsInput.substring(matchingIndex, matchingIndex+30));
-  pointer = matchingIndex + "mul(".length;
+  matchingIndex = instructionsInput.indexOf(startMul, pointer) + startMul.length;
+  instructionsPart1 = instructionsInput.substring(matchingIndex, matchingIndex + maxPattern.length);
+  console.log(instructionsPart1);
+  pointer = matchingIndex;
 }
