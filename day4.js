845dab42d54ed5d19c7f5ea9e862290c3729defb
BHAVIK KAWLI <37347846+kawxbhavx@users.noreply.github.com>
2024-12-04 21:23:05 +0530

    Update day4.js

--- day4.js
+++ day4.js
@@ -1,1 +1,9 @@
-
+//part1
+patternInput = document.querySelector('pre').innerText;
+pattern = patternInput.split("\n");
+pattern.pop();
+for(let row = 0;row<pattern.length;row++) {
+  for(let col = 0;col<pattern[0].length;col++) {
+    console.log(pattern[row][col]);
+  }
+}
