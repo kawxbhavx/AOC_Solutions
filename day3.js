f327edb4d195c1840667d8a1df23cb6721a3ab5c
BHAVIK KAWLI <37347846+kawxbhavx@users.noreply.github.com>
2024-12-04 19:28:01 +0530

    starting part2

--- day3.js
+++ day3.js
@@ -1,5 +1,5 @@
 //part1
-instructionsInput = document.querySelector('pre').innerText;
+/*instructionsInput = document.querySelector('pre').innerText;
 instructions = [];
 let pointer=0;
 //debugger;
@@ -10,4 +10,18 @@ while(pointer<instructionsInput.length) {
   instructionsPart1 = instructionsInput.substring(matchingIndex, matchingIndex + maxPattern.length);
   console.log(instructionsPart1);
   pointer = matchingIndex;
-}
+}*/
+instructionsInput = document.querySelector('pre').innerText;
+instructions = instructionsInput.match(/mul\([0-9]{1,3},[0-9]{1,3}\)/g);
+result = 0;
+instructions.forEach(function(instruction) {
+  instruction = instruction.replace("mul(","");
+  instruction = instruction.replace(")","");
+  params = instruction.split(",");
+  number1 = parseInt(params[0]);
+  number2 = parseInt(params[1]);
+  result = result + (number1*number2);
+});
+console.log(result);
+
+//part2
