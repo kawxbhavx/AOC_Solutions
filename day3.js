f5c7b2bb22e05a3742a2e39d35eaca60067926fc
BHAVIK KAWLI <37347846+kawxbhavx@users.noreply.github.com>
2024-12-04 20:10:31 +0530

    day3 part2

--- day3.js
+++ day3.js
@@ -25,3 +25,24 @@ instructions.forEach(function(instruction) {
 console.log(result);
 
 //part2
+instructionsInput = document.querySelector('pre').innerText;
+instructions = instructionsInput.match(/do\(\)|don't\(\)|mul\([0-9]{1,3},[0-9]{1,3}\)/g);
+result = 0;
+enabled=true;
+instructions.forEach(function(instruction) {
+  if(instruction === "do()") {
+    enabled=true;
+  } else if(instruction === "don't()") {
+    enabled=false;
+  } else {
+    if(enabled) {
+      instruction = instruction.replace("mul(","");
+      instruction = instruction.replace(")","");
+      params = instruction.split(",");
+      number1 = parseInt(params[0]);
+      number2 = parseInt(params[1]);
+      result = result + (number1*number2);
+    }
+  }
+});
+console.log(result);
