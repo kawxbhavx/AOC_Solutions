9a988df59d92885dd35002b51f68135fea9238f9
BHAVIK KAWLI <37347846+kawxbhavx@users.noreply.github.com>
2024-12-05 10:53:22 +0530

    Update day4.js

--- day4.js
+++ day4.js
@@ -107,18 +107,14 @@ for(let col = 0;col<patternRows[0].length;col++) {
   pattern.push(colStr);
 }
 for(let row = 0;row<patternRows.length-3;row++) {
-  diagStr = "";
-  for(let col = 0;col<patternRows[row].length-4;col++) {
-    diagStr = diagStr + patternRows[row][col] + patternRows[row+1][col+1] + patternRows[row+2][col+2] + patternRows[row+3][col+3];
-  }
-  pattern.push(diagStr);
+  for(let col = 0;col<patternRows[row].length-3;col++) {
+    pattern.push(patternRows[row][col] + patternRows[row+1][col+1] + patternRows[row+2][col+2] + patternRows[row+3][col+3]);
+  }  
 }
 for(let row = 0;row<patternRows.length-4;row++) {
-  diagStr = "";
-  for(let col = 3;col<patternRows[row].length;col++) {
-    diagStr = patternRows[row+3][col-3] + patternRows[row+2][col-2] + patternRows[row+1][col-1] + patternRows[row][col] + diagStr;
+  for(let col = 3;col<patternRows[row].length-1;col++) {
+      pattern.push(patternRows[row+3][col-3] + patternRows[row+2][col-2] + patternRows[row+1][col-1] + patternRows[row][col]);
   }
-  pattern.push(diagStr);
 }
 patternStr = pattern.join("");
 console.log(patternStr.match(/XMAS|SAMX/g).length);
