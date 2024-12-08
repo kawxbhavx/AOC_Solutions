5b2f5708736188893202c577f4482951304f9269
BHAVIK KAWLI <37347846+kawxbhavx@users.noreply.github.com>
2024-12-08 10:29:13 +0530

    Update day5.js

--- day5.js
+++ day5.js
@@ -1,5 +1,6 @@
 //part1
-patternInput = "47|53\n97|13\n97|61\n97|47\n75|29\n61|13\n75|53\n29|13\n97|29\n53|29\n61|53\n97|53\n61|29\n47|13\n75|47\n97|75\n47|61\n75|61\n47|29\n75|13\n53|13\n\n75,47,61,53,29\n97,61,53,29,13\n75,29,13\n75,97,47,61,53\n61,13,29\n97,13,75,29,47\n";
+patternInput = "47|53\n97|13\n97|61\n97|47\n75|29\n61|13\n75|53\n29|13\n97|29\n53|29\n61|53\n97|53\n61|29\n47|13\n75|47\n97|75\n47|61\n75|61\n47|29\n75|13\n53|13\n\n" + 
+  "75,47,61,53,29\n97,61,53,29,13\n75,29,13\n75,97,47,61,53\n61,13,29\n97,13,75,29,47\n";
 patternInput = document.querySelector('pre').innerText;
 pattern = patternInput.split("\n");
 let rulesStr = [];
@@ -15,7 +16,6 @@ for(let row=0;row<pattern.length-1;row++) {
 }
 
 let rules=[];
-let sortedRules=[];
 rulesStr.forEach(function(rule) {
   let ruleList = [];
   rule.split("|").forEach(function(ruleEntry) {
@@ -32,6 +32,40 @@ updatesStr.forEach(function(update) {
   updates.push(updateList);
 });
 
+let validUpdates=[];
+updates.forEach(function(updateList) {
+  debugger;
+  let ruleViolated=false;
+  for(let currentUpdateIndex=0;currentUpdateIndex<updateList.length-1;currentUpdateIndex++) {    
+    let currentUpdate=updateList[currentUpdateIndex];
+    for(let nextUpdateIndex=1;nextUpdateIndex<updateList.length;nextUpdateIndex++) {
+      let nextUpdate=updateList[nextUpdateIndex];
+      for(let ruleIndex=0;ruleIndex<rules.length;ruleIndex++) {
+        let dependency=rules[ruleIndex][0];
+        let dependent=rules[ruleIndex][1];
+        if(dependent===currentUpdate) {
+          if(dependency===nextUpdate) {
+            ruleViolated=true;
+            break;
+          }
+        }
+      }
+      if(ruleViolated) {
+        break;
+      }
+    }
+    if(ruleViolated) {
+        break;
+      }
+  } 
+  if(!ruleViolated) {
+    validUpdates.push(updateList);
+  }
+});
+
+
+
+let sortedRules=[];
 for(let ruleIndex=0;ruleIndex<rules.length-1;ruleIndex++) {
   let dependency=rules[ruleIndex][0];
   let dependent=rules[ruleIndex][1];
@@ -43,19 +77,3 @@ for(let ruleIndex=0;ruleIndex<rules.length-1;ruleIndex++) {
     sortedRules.push(currentRule);
   }
 }
-
-
-let validUpdates=[];
-updates.forEach(function(updateList) {
-  for(let currentUpdateIndex=1;currentUpdateIndex<updateList.length;currentUpdateIndex++) {
-    let isValidUpdate=true;
-    for(let previousUpdateIndex=0;previousUpdateIndex<currentUpdateIndex;previousUpdateIndex) {
-      for(int ruleIndex=0;ruleIndex<rules.length-1;ruleIndex++) {
-       if(rules[ruleIndex][1]===updateList[currentUpdateIndex]) {
-         
-       }
-      }
-    }
-  }  
-});
-                
