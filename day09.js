8b2e2ab8c585fbc6ec2c241b06066191f325be63
BHAVIK KAWLI <37347846+kawxbhavx@users.noreply.github.com>
2024-12-11 23:01:09 +0530

    Update day09.js

--- day09.js
+++ day09.js
@@ -44,6 +44,7 @@ for(let index=0;index<fileBlocks.length;index++) {
 
 //part2
 input = document.querySelector('pre').innerText;
+input = "2333133121414131402\n";
 pattern = input.split("\n");
 pattern.pop();
 pattern = pattern[0];
@@ -65,4 +66,31 @@ for(let index=0;index<pattern.length;index++) {
     fileBlocks.push(".".toString().repeat(mapDigit).split(""));
   }
 }
-//let fileBlocks="00...111...2...333.44.5555.6666.777.888899".split("");
+
+let fileId=0;
+for(let blockIndex=fileBlocks.length-1;blockIndex>=0;blockIndex--) {
+  if(!fileBlocks[blockIndex].includes(".")) {
+    fileId = fileBlocks[blockIndex][0];
+    break;
+  }
+}
+
+for(fileId;fileId>0;fileId--) {
+  let blockIndex=fileBlocks.length-1;
+  //debugger;
+  while(!fileBlocks[blockIndex].includes(fileId)) {
+    blockIndex--;
+  }
+  let blockLen=fileBlocks[blockIndex].length;
+  for(let spaceIndex=1;spaceIndex<fileBlocks.length;spaceIndex++) {
+    if(fileBlocks[spaceIndex].includes(".")) {
+      if(fileBlocks[spaceIndex].length>=blockLen) {
+        fileBlocks.splice(spaceIndex,0,fileBlocks[blockIndex]);
+        fileBlocks[spaceIndex+1].splice(0,blockLen);
+        blockIndex++;
+        fileBlocks.splice(blockIndex,Infinity);        
+        break;
+      }
+    }
+  }
+}
