5bf1092c2a056d39aa0cb674fa46e56113245e08
BHAVIK KAWLI <37347846+kawxbhavx@users.noreply.github.com>
2024-12-11 16:00:50 +0530

    Update day10.js

--- day10.js
+++ day10.js
@@ -35,7 +35,7 @@ for(let row=0;row<rows;row++) {
     if(pattern[row][col]===0) {
       let patternCopy=generateBlankPattern();
       patternCopy[row][col]=0;
-      trails[trailHeadNumber] = {"trailPathPattern":patternCopy, "pathPositions":[{"pathPositionDir":[0],"positionCoordinates":[[row,col]]}]};
+      trails[trailHeadNumber] = {"trailPathPattern":patternCopy, "pathPositions":[{"pathPositionDir":[0],"positionRows":[row],"positionCols":[col]}]};
       trailHeadNumber++;
     }
   }
@@ -44,33 +44,41 @@ for(let row=0;row<rows;row++) {
 for(let currentHeight=0;currentHeight<=8;currentHeight++) {
   for(let trailHeadNumber in trails) {
     trails[trailHeadNumber].pathPositions.forEach(function(pathPosition) {
-      let previousPositions=pathPosition.positionCoordinates;
-      let positionRow=previousPositions[previousPositions.length-1][0];
-      let positionCol=previousPositions[previousPositions.length-1][1];
+      //let previousPositions=pathPosition.positionCoordinates;
+      let positionRow=pathPosition.positionRows[pathPosition.positionRows.length-1];
+      let positionCol=pathPosition.positionCols[pathPosition.positionCols.length-1];
       if(currentHeight===pattern[positionRow][positionCol]) {
         let currentPositionDir=pathPosition.pathPositionDir[pathPosition.pathPositionDir.length-1];
         if(positionRow>=1) {
           if(pattern[positionRow-1][positionCol]===(currentHeight+1)) {
             trails[trailHeadNumber].trailPathPattern[positionRow-1][positionCol]=currentHeight+1;
-            trails[trailHeadNumber].pathPositions.push({"pathPositionDir":[...pathPosition.pathPositionDir,1],"positionCoordinates":[previousPositions,[positionRow-1,positionCol]]});
+            trails[trailHeadNumber].pathPositions.push({"pathPositionDir":[...pathPosition.pathPositionDir,1],
+                                                        "positionRows":[...pathPosition.positionRows,positionRow-1],
+                                                        "positionCols":[...pathPosition.positionCols,positionCol]});
           }
         }
         if(positionRow<=rows-2) {
           if(pattern[positionRow+1][positionCol]===(currentHeight+1)) {
             trails[trailHeadNumber].trailPathPattern[positionRow+1][positionCol]=currentHeight+1;
-            trails[trailHeadNumber].pathPositions.push({"pathPositionDir":[...pathPosition.pathPositionDir,2],"positionCoordinates":[previousPositions,[positionRow+1,positionCol]]});
+            trails[trailHeadNumber].pathPositions.push({"pathPositionDir":[...pathPosition.pathPositionDir,2],
+                                                        "positionRows":[...pathPosition.positionRows,positionRow+1],
+                                                        "positionCols":[...pathPosition.positionCols,positionCol]});
           }
         }
         if(positionCol>=1) {
           if(pattern[positionRow][positionCol-1]===(currentHeight+1)) {
             trails[trailHeadNumber].trailPathPattern[positionRow][positionCol-1]=currentHeight+1;
-            trails[trailHeadNumber].pathPositions.push({"pathPositionDir":[...pathPosition.pathPositionDir,3],"positionCoordinates":[previousPositions,[positionRow,positionCol-1]]});
+            trails[trailHeadNumber].pathPositions.push({"pathPositionDir":[...pathPosition.pathPositionDir,3],
+                                                        "positionRows":[...pathPosition.positionRows,positionRow],
+                                                        "positionCols":[...pathPosition.positionCols,positionCol-1]});
           }
         }        
         if(positionCol<=cols-2) {
           if(pattern[positionRow][positionCol+1]===(currentHeight+1)) {
             trails[trailHeadNumber].trailPathPattern[positionRow][positionCol+1]=currentHeight+1;
-            trails[trailHeadNumber].pathPositions.push({"pathPositionDir":[...pathPosition.pathPositionDir,4],"positionCoordinates":[previousPositions,[positionRow,positionCol+1]]});
+            trails[trailHeadNumber].pathPositions.push({"pathPositionDir":[...pathPosition.pathPositionDir,4],
+                                                        "positionRows":[...pathPosition.positionRows,positionRow],
+                                                        "positionCols":[...pathPosition.positionCols,positionCol+1]});
           }
         }
       }
@@ -79,12 +87,34 @@ for(let currentHeight=0;currentHeight<=8;currentHeight++) {
 }
 
 let score=0;
-for(let trailHeadNumber in trails) {
+
+for(let trailHeadNumber in trails) {  
   let trailPathPattern = trails[trailHeadNumber].trailPathPattern;
   trailPathPattern.forEach(function(pathRow, index) {
     trailPathPattern[index]=pathRow.join("");
   });
-  trails[trailHeadNumber]=trailPathPattern.join("\n");
-  score = score + trails[trailHeadNumber].match(/9/g).length;
+  trails[trailHeadNumber].trailPathPattern=trailPathPattern.join("\n");
+  score = score + trails[trailHeadNumber].trailPathPattern.match(/9/g).length;
 }
 score;
+
+//part2
+let maxTrailLength=0;
+for(let trailHeadNumber in trails) {
+if(trails[trailHeadNumber].pathPositions[trails[trailHeadNumber].pathPositions.length-1].positionRows.length>maxTrailLength) {
+    maxTrailLength=trails[trailHeadNumber].pathPositions[trails[trailHeadNumber].pathPositions.length-1].positionRows.length;
+  }
+}
+
+let uniquePaths=[];
+for(let trailHeadNumber in trails) {
+  trails[trailHeadNumber].pathPositions.forEach(function(trailPath) {
+    if(trailPath.positionRows.length===maxTrailLength) {
+      let trailPathStr=trailPath.positionRows.join("") + trailPath.positionCols.join("");
+      if(!uniquePaths.includes(trailPathStr)) {
+        uniquePaths.push(trailPathStr);
+      }
+    }
+  });
+}
+uniquePaths.length;
