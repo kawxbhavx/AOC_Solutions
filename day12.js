a395e2014359c1eab38caf0099ea73e601c85884
BHAVIK KAWLI <37347846+kawxbhavx@users.noreply.github.com>
2024-12-12 21:39:56 +0530

    Update day12.js

--- day12.js
+++ day12.js
@@ -1,3 +1,4 @@
+//part1 approach 1
 let preNode=document.querySelector('pre');
 //preNode.innerText = fieldInput;
 let fieldInput = preNode.innerText;
@@ -60,3 +61,40 @@ for(let plantType in plantTypes) {
   price = price + (plantTypes[plantType] * perimeters[plantType]);
 }
 price;
+
+
+//part1 approach 2
+//let preNode=document.querySelector('pre');
+//preNode.innerText = fieldInput;
+//let fieldInput = preNode.innerText;
+//let fieldInput = "AAAA\nBBCD\nBBCC\nEEEC\n";
+//let fieldInput = "OOOOO\nOXOXO\nOOOOO\nOXOXO\nOOOOO\n";
+let fieldInput = "RRRRIICCFF\nRRRRIICCCF\nVVRRRCCFFF\nVVRCCCJFFF\nVVVVCJJCFE\nVVIVCCJJEE\nVVIIICJJEE\nMIIIIIJJEE\nMIIISIJEEE\nMMMISSJEEE\n";
+fieldRows = fieldInput.split("\n");
+fieldRows.pop();
+function generatePattern(patternRows) {
+  let pattern=[];
+  for(let row=0;row<patternRows.length;row++) {
+    pattern.push(patternRows[row].split(""));
+  }
+  return pattern;
+}
+
+let pattern = generatePattern(fieldRows);
+let plantTypes={};
+let perimeters={};
+let rows=pattern.length;
+let cols=pattern[0].length;
+for(let row=0;row<rows;row++) {
+  for(let col=0;col<cols;col++) {
+    if(plantTypes.hasOwnProperty(pattern[row][col])) {
+      plantTypes[pattern[row][col]]=plantTypes[pattern[row][col]]+1;
+    } else {
+      plantTypes[pattern[row][col]]=1;
+    }    
+  }
+}
+
+
+
+
