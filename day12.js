3da0abeb7f4374b752b31945a4918926666df2d5
BHAVIK KAWLI <37347846+kawxbhavx@users.noreply.github.com>
2024-12-12 20:17:33 +0530

    Update day12.js

--- day12.js
+++ day12.js
@@ -1,9 +1,9 @@
-//let preNode=document.querySelector('pre');
-// preNode.innerText = fieldInput;
-//let fieldInput = preNode.innerText;
+let preNode=document.querySelector('pre');
+//preNode.innerText = fieldInput;
+let fieldInput = preNode.innerText;
 //let fieldInput = "AAAA\nBBCD\nBBCC\nEEEC\n";
 //let fieldInput = "OOOOO\nOXOXO\nOOOOO\nOXOXO\nOOOOO\n";
-let fieldInput = "RRRRIICCFF\nRRRRIICCCF\nVVRRRCCFFF\nVVRCCCJFFF\nVVVVCJJCFE\nVVIVCCJJEE\nVVIIICJJEE\nMIIIIIJJEE\nMIIISIJEEE\nMMMISSJEEE\n";
+//let fieldInput = "RRRRIICCFF\nRRRRIICCCF\nVVRRRCCFFF\nVVRCCCJFFF\nVVVVCJJCFE\nVVIVCCJJEE\nVVIIICJJEE\nMIIIIIJJEE\nMIIISIJEEE\nMMMISSJEEE\n";
 fieldRows = fieldInput.split("\n");
 fieldRows.pop();
 function generatePattern(patternRows) {
@@ -14,10 +14,11 @@ function generatePattern(patternRows) {
   return pattern;
 }
 
-pattern = generatePattern(fieldRows);
-plantTypes={};
-rows=pattern.length;
-cols=pattern[0].length;
+let pattern = generatePattern(fieldRows);
+let plantTypes={};
+let perimeters={};
+let rows=pattern.length;
+let cols=pattern[0].length;
 for(let row=0;row<rows;row++) {
   for(let col=0;col<cols;col++) {
     if(plantTypes.hasOwnProperty(pattern[row][col])) {
@@ -25,5 +26,37 @@ for(let row=0;row<rows;row++) {
     } else {
       plantTypes[pattern[row][col]]=1;
     }
+    let cellPerimeter=4;
+    if(row>0) {
+      if(pattern[row][col]===pattern[row-1][col]) {
+        cellPerimeter--;
+      }
+    }
+    if(row<rows-1) {
+      if(pattern[row][col]===pattern[row+1][col]) {
+        cellPerimeter--;
+      }
+    }
+    if(col>0) {
+      if(pattern[row][col]===pattern[row][col-1]) {
+        cellPerimeter--;
+      }
+    }
+    if(col<cols-1) {
+      if(pattern[row][col]===pattern[row][col+1]) {
+        cellPerimeter--;
+      }
+    }
+    if(perimeters.hasOwnProperty(pattern[row][col])) {
+      perimeters[pattern[row][col]]=perimeters[pattern[row][col]]+cellPerimeter;
+    } else {
+      perimeters[pattern[row][col]]=cellPerimeter;
+    }
   }
 }
+
+let price=0;
+for(let plantType in plantTypes) {
+  price = price + (plantTypes[plantType] * perimeters[plantType]);
+}
+price;
