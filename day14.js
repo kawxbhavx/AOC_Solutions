d42ac2c089d459ebe6bf5622c3dd290137c7d880
BHAVIK KAWLI <37347846+kawxbhavx@users.noreply.github.com>
2024-12-14 16:27:04 +0530

    Create day14.js

---
+++ day14.js
@@ -0,0 +1,22 @@
+//part1
+let preNode=document.querySelector('pre');
+//preNode.innerText = fieldInput;
+let robotDetailsStr = preNode.innerText;
+//let robotDetailsStr = "p=0,4 v=3,-3" + 
+"\np=6,3 v=-1,-3" +
+"\np=10,3 v=-1,2" +
+"\np=2,0 v=2,-1" +
+"\np=0,0 v=1,3" +
+"\np=3,0 v=-2,-2" +
+"\np=7,6 v=-1,-3" +
+"\np=3,0 v=-1,-2" +
+"\np=9,3 v=2,3" +
+"\np=7,3 v=-1,2" +
+"\np=2,4 v=2,-3" +
+"\np=9,5 v=-3,-3";
+//let tileRows=7;
+//let tileCols=11;
+
+let tileRows=103;
+let tileCols=101;
+
