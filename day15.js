2c1d55f7bea524e59242b77401c5c68afd17acec
BHAVIK KAWLI <37347846+kawxbhavx@users.noreply.github.com>
2024-12-15 18:53:50 +0530

    Update day15.js

--- day15.js
+++ day15.js
@@ -3,7 +3,7 @@ let patternInput = document.querySelector('pre').innerText;
 let warehouse=patternInput.substring(0,patternInput.lastIndexOf("#")+1);
 let moves=patternInput.substring(patternInput.lastIndexOf("#")+1);
 
-//let warehouse="##########"+
+let warehouse="##########"+
 "\n#..O..O.O#"+
 "\n#......O.#"+
 "\n#.OO..O.O#"+
@@ -14,7 +14,7 @@ let moves=patternInput.substring(patternInput.lastIndexOf("#")+1);
 "\n#....O...#"+
 "\n##########";
 
-//let moves="<vv>^<v^>v>^vv^v>v<>v^v<v<^vv<<<^><<><>>v<vvv<>^v^>^<<<><<v<<<v^vv^v>^"+
+let moves="<vv>^<v^>v>^vv^v>v<>v^v<v<^vv<<<^><<><>>v<vvv<>^v^>^<<<><<v<<<v^vv^v>^"+
 "\nvvv<<^>^v^^><<>>><>^<<><^vv^^<>vvv<>><^^v>^>vv<>v<<<<v<^v>^<^^>>>^<v<v"+
 "\n><>vv>v^v^<>><>>>><^^>vv>v<^^^>>v^v^<^^>v^^>v^<^v>v<>>v^v^<v>v^^<^^vv<"+
 "\n<<v<^>>^^^^>>>v^<>vvv^><v<<<>^^^vv^<vvv>^>v<^^^^v<>^>vvvv><>>v^<<^^^^^"+
@@ -25,7 +25,7 @@ let moves=patternInput.substring(patternInput.lastIndexOf("#")+1);
 "\n^^>vv<^v^v<vv>^<><v<^v>^^^>>>^^vvv^>vvv<>>>^<^>>>>>^<<^v>^vvv<>^<><<v>"+
 "\nv^^>>><<^^<>>^v^<v^vv<>v^<<>^<^v^v><^<<<><<^<v><v<>vv>>v><v^<vv<>v^<<^\n";
 
-let warehouse="########"+
+//let warehouse="########"+
 "\n#..O.O.#"+
 "\n##@.O..#"+
 "\n#...O..#"+
@@ -34,16 +34,7 @@ let warehouse="########"+
 "\n#......#"+
 "\n########";
 
-let warehouse="########"+
-"\n#..O.O.#"+
-"\n#.#.O@.#"+
-"\n#...O..#"+
-"\n#.#.O..#"+
-"\n#...O..#"+
-"\n#......#"+
-"\n########";
-
-let moves="<^^>>>vv<v>>v<<\n";
+//let moves="<^^>>>vv<v>>v<<\n";
 
 moves=moves.replaceAll("\n","");
 
@@ -76,44 +67,76 @@ for(startRow=0;startRow<rows;startRow++) {
   }
 }
 
-
-
 for(let i=0;i<moves.length;i++) {
   if(moves[i]==="<") {
-    let warehouseRow=warehousePattern[startRow];
-    let nearestWallIndex=warehouseRow.lastIndexOf(wall,warehouseRow.lastIndexOf(robot)-1);
-    for(let col=0;col<nearestWallIndex;col++) {
-      if(warehouseRow[col]===space) {
-        warehouseRow[col]=imaginaryWall;
-      }
-    }
-    let nearestSpaceIndex=warehouseRow.lastIndexOf(space,warehouseRow.lastIndexOf(robot)-1);
-    if(nearestSpaceIndex>0) {
-      let hasBox=false;
-      for(let col=nearestSpaceIndex;col<startCol;col++) {
-        if(warehouseRow[col]===box) {
-          hasBox=true;
-          break;
-        }
-      }
-      if(hasBox) {
-        warehouseRow[nearestSpaceIndex]=box;
-      }
-      warehouseRow[startCol]=space;
-      startCol--;
-      warehouseRow[startCol]=robot;
-    }
-    for(let col=0;col<cols;col++) {
-      if(warehouseRow[col]===imaginaryWall) {
-        warehouseRow[col]=space;
-      }
-    }
-    debugger;
+    let warehouseRow=[...warehousePattern[startRow]];
+    generateMove(warehouseRow);
+    warehouseRow.forEach(function(element,index) {
+      warehousePattern[startRow][index]=element;
+    });
+    startCol=warehouseRow.indexOf(robot);
   } else if(moves[i]==="^") {
-    
+    let warehouseRow=[];
+    for(let row=0;row<rows;row++) {
+      warehouseRow.push(warehousePattern[row][startCol]);
+    }
+    generateMove(warehouseRow);
+    warehouseRow.forEach(function(element,index) {
+      warehousePattern[index][startCol]=element;
+    });
+    startRow=warehouseRow.indexOf(robot);
   } else if(moves[i]===">") {
-    
+    let warehouseRow=[...warehousePattern[startRow]];
+    warehouseRow.reverse();
+    generateMove(warehouseRow);
+    warehouseRow.reverse();
+    warehouseRow.forEach(function(element,index) {
+      warehousePattern[startRow][index]=element;
+    });
+    startCol=warehouseRow.indexOf(robot);
   } else if(moves[i]==="v") {
-    
+    let warehouseRow=[];
+    for(let row=0;row<rows;row++) {
+      warehouseRow.push(warehousePattern[row][startCol]);
+    }
+    warehouseRow.reverse();
+    generateMove(warehouseRow);
+    warehouseRow.reverse();
+    warehouseRow.forEach(function(element,index) {
+      warehousePattern[index][startCol]=element;
+    });
+    startRow=warehouseRow.indexOf(robot);
   }
 }
+
+function generateMove(patternRow) {
+  let nearestWallIndex=patternRow.lastIndexOf(wall,patternRow.lastIndexOf(robot)-1);
+  for(let col=0;col<nearestWallIndex;col++) {
+    if(patternRow[col]===space) {
+      patternRow[col]=imaginaryWall;
+    }
+  }
+  let nearestSpaceIndex=patternRow.lastIndexOf(space,patternRow.lastIndexOf(robot)-1);
+  if(nearestSpaceIndex>0) {
+    let hasBox=false;
+    let robotCol=patternRow.indexOf(robot);
+    for(let col=nearestSpaceIndex;col<robotCol;col++) {
+      if(patternRow[col]===box) {
+        hasBox=true;
+        break;
+      }
+    }
+    if(hasBox) {
+      patternRow[nearestSpaceIndex]=box;
+    }
+    patternRow[robotCol]=space;
+    robotCol--;    
+    patternRow[robotCol]=robot;
+  }
+  for(let col=0;col<cols;col++) {
+    if(patternRow[col]===imaginaryWall) {
+      patternRow[col]=space;
+    }
+  }
+}
+  
