d482996ca7da0c3162c7fcce4e8fe8d307c4d4da
BHAVIK KAWLI <37347846+kawxbhavx@users.noreply.github.com>
2024-12-15 08:53:03 +0530

    Update day06.js

--- day06.js
+++ day06.js
@@ -210,3 +210,81 @@ for(let row=0;row<patternField.length;row++) {
   }
 }
 
+//part1 refactored
+patternInput = document.querySelector('pre').innerText;
+patternRows = patternInput.split("\n");
+patternRows.pop();
+let pattern=[];
+for(let row=0;row<patternRows.length;row++) {
+  pattern.push(patternRows[row].split(""));
+}
+
+let rows=pattern.length;
+let cols=pattern[0].length;
+
+let row=0;
+let col=0;
+let direction="up";
+
+for(row=0;row<pattern.length;row++) {
+  let rowFound=false;
+  for(col=0;col<pattern[row].length;col++) {
+    if(pattern[row][col]==='^') {
+      rowFound=true;
+      //pattern[row][col] = "X";
+      break;
+    }
+  }
+  if(rowFound) {
+    break;
+  }
+}
+
+while(row>=0 && row<pattern.length && col>=0 && col<pattern[0].length) {
+  //try {
+    if(direction==="up") {
+      if(row>0) {
+        if(pattern[row-1][col]==="#") {
+          direction="right";
+        } else {          
+          pattern[row][col] = "X";      
+          row--;
+        }
+      } else {
+        break;
+      }
+    } else if(direction==="right") {
+      if(col<cols-1) {
+        if(pattern[row][col+1]==="#") {
+          direction="down";
+        } else {
+          col++;
+          pattern[row][col] = "X";      
+        }
+      }
+    } else if(direction==="down") {
+      if(pattern[row+1][col]==="#") {
+        direction="left";
+      } else {
+        row++;      
+        pattern[row][col] = "X";
+      }
+    } else if(direction==="left") {
+      if(pattern[row][col-1]==="#") {
+        direction="up";
+      } else {
+        col--;
+        pattern[row][col] = "X";      
+      }
+    }
+  //} catch (err) {
+    //break;
+  //}
+}
+
+for(let row=0;row<pattern.length;row++) {
+  pattern[row] = pattern[row].join("");
+}
+pattern = pattern.join("\n");
+document.querySelector('pre').innerText = pattern;
+pattern.match(/X/g).length;
